64af182d5e40a2e36d43ec6bdde7633fe32d3392
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-09-25 22:24:03 +0200

    Do not require fpdb config file

--- exttextcat.el
+++ exttextcat.el
@@ -1,6 +1,8 @@
 (defvar exttextcat-executable (exttextcat-path-for-file-in-project "exttextcat"))
 
-(defvar exttextcat-fpdb-config (exttextcat-path-for-file-in-project "fpdb.conf"))
+(defvar exttextcat-language-models
+  '(("/usr/share/libexttextcat/en.lm" . "english")
+    ("/usr/share/libexttextcat/pl.lm" . "polish")))
 
 ;;;###autoload
 (defun exttextcat-guess-language-buffer ()
@@ -28,10 +30,22 @@
                                      nil
                                      (current-buffer)
                                      nil
-                                     exttextcat-fpdb-config
+                                     (exttextcat-build-fpdb-config-file)
                                      input-file))
           (cmd-output (buffer-string)))
       (cons exit-status cmd-output))))
+
+(defun exttextcat-build-fpdb-config-file ()
+  (let ((temp-file (make-temp-file "fpdb.conf")))
+    (with-temp-file temp-file
+      (insert (exttextcat-build-fpdb-config)))
+    temp-file))
+
+(defun exttextcat-build-fpdb-config ()
+  (mapconcat (lambda (model-language)
+               (concat (car model-language) " " (cdr model-language)))
+             exttextcat-language-models
+             "\n"))
 
 (defun exttextcat-path-for-file-in-project (filename)
   (let* ((script-file (or load-file-name buffer-file-name))
