c07a24a67ec29810d7473bfe8ea21e02da54b41e
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-09-25 19:50:52 +0200

    Handle exit code and buffer not visiting file

--- exttextcat.el
+++ exttextcat.el
@@ -9,18 +9,29 @@
     (ispell-change-dictionary language)))
 
 (defun exttextcat-get-language-for-buffer ()
-  (let ((lang-output (exttextcat-call-c-wrapper)))
-    (exttextcat-extract-lang lang-output)))
+  (let* ((exit-status-output (exttextcat-call-c-wrapper))
+         (exit-status (car exit-status-output))
+         (output (cdr exit-status-output)))
+    (unless (zerop exit-status)
+      (error "Program exited with %d exit code" exit-status))
+    (exttextcat-extract-lang output)))
 
 (defun exttextcat-extract-lang (lang-output)
   (substring lang-output 1 (- (length lang-output) 2)))
 
-(defun exttextcat-call-c-wrapper ()
-  (shell-command-to-string
-   (format "%s %s %s"
-           exttextcat-executable
-           exttextcat-fpdb-config
-           buffer-file-name)))
+(defun exttextcat-call-c-wrapper (&optional input-file)
+  (setq input-file (or input-file buffer-file-name))
+  (unless input-file
+    (error "Buffer is not visiting file"))
+  (with-temp-buffer
+    (let ((exit-status (call-process exttextcat-executable
+                                     nil
+                                     (current-buffer)
+                                     nil
+                                     exttextcat-fpdb-config
+                                     input-file))
+          (cmd-output (buffer-string)))
+      (cons exit-status cmd-output))))
 
 (defun exttextcat-path-for-file-in-project (filename)
   (let* ((script-file (or load-file-name buffer-file-name))
