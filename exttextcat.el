8563e9d5ac03ae520ab910c4653fd16fafb48f9b
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2014-09-28 13:37:00 +0200

    Add command to build wrapper and make if exists before using

--- exttextcat.el
+++ exttextcat.el
@@ -1,4 +1,11 @@
-(defvar exttextcat-executable (exttextcat-path-for-file-in-project "exttextcat"))
+(defvar exttextcat-executable (concat exttextcat-build-dir "exttextcat"))
+
+(defvar exttextcat-build-dir (locate-user-emacs-file "exttextcat/"))
+
+(defvar exttextcat-package-dir
+  (let ((script-file (or load-file-name
+                         buffer-file-name))) ; For direct evaluation
+    (file-name-directory script-file)))
 
 (defvar exttextcat-language-models
   '(("/usr/share/libexttextcat/en.lm" . "english")
@@ -28,6 +35,8 @@
   (setq input-file (or input-file buffer-file-name))
   (unless input-file
     (error "Buffer is not visiting file"))
+  (unless (file-executable-p exttextcat-executable)
+    (error "Call `exttextcat-install-wrapper' command to build library wrapper"))
   (with-temp-buffer
     (let ((exit-status (call-process exttextcat-executable
                                      nil
@@ -51,9 +60,13 @@
              exttextcat-language-models
              "\n"))
 
-(defun exttextcat-path-for-file-in-project (filename)
-  (let* ((script-file (or load-file-name buffer-file-name))
-         (script-directory (file-name-directory script-file)))
-    (concat script-directory filename)))
+;;;###autoload
+(defun exttextcat-install-wrapper ()
+  (interactive)
+  (let ((default-directory exttextcat-package-dir)
+        (build-cmd (concat "make DESTDIR=" exttextcat-build-dir)))
+    (make-directory exttextcat-build-dir t)
+    (compilation-start build-cmd nil (lambda (mode-name)
+                                       "*exttextcat-compilation*"))))
 
 (provide 'exttextcat)
